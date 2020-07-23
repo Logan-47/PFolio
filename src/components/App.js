c62c96cdcdf7ffb17e60d8509220f88ab0268145
Logan-47 <ashishofficial47@gmail.com>
2020-07-23 17:27:48 +0530

    intial setup-2

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Landing from './Landing';
+
+const App = () => {
+    return (
+        <div>
+            <Landing /> 
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div className="landing">
+            <div className="overlay">
+                Hello World
+            </div>
+        </div>
+    )
+}
+
+export default Landing
