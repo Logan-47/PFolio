8f459998496fab3692ab17eb18d6a3298aa3031c
Logan-47 <Logan-47@users.noreply.github.com>
2020-07-23 12:10:20 +0000

    deploy: feb5ad88ae5b3564cd52e0cf1cea42525ff1528c

---
+++ precache-manifest.5d024100e294c79647c80c97a3a45b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9727760a2f1768d231fcf1a99811832c",
+    "url": "/pfolio/index.html"
+  },
+  {
+    "revision": "d4ba98a0d9c53a69778d",
+    "url": "/pfolio/static/css/main.9b55fb3e.chunk.css"
+  },
+  {
+    "revision": "cf8e1ee25d842135d7d8",
+    "url": "/pfolio/static/js/2.d0522eae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pfolio/static/js/2.d0522eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ba98a0d9c53a69778d",
+    "url": "/pfolio/static/js/main.28fe7f1c.chunk.js"
+  },
+  {
+    "revision": "da6eb8c291b1f39d1e9c",
+    "url": "/pfolio/static/js/runtime-main.152cfca7.js"
+  },
+  {
+    "revision": "a597ee70e1323361023fb543331f7d50",
+    "url": "/pfolio/static/media/main-bg.a597ee70.jpg"
+  }
+]);
